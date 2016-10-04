2cc6b08a304964d02675e180d615d56a3f48762a
Nate Tangsurat <e4r7hbug@gmail.com>
2016-10-03 22:21:04 -0500

    feat: Add logging

--- main.go
+++ main.go
@@ -5,9 +5,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/Sirupsen/logrus"
 )
+
+var (
+	log = logrus.New()
+)
+
+func init() {
+	logrus.SetLevel(logrus.DebugLevel)
+	log.Level = logrus.DebugLevel
+}
 
 func main() {
 	fmt.Printf("Hellow!\n")
+
+	log.Debug("This is DEBUG.")
+	log.Info("This is INFO.")
+	log.Warning("This is WARNING.")
+	log.Error("This is ERROR.")
+
+	if false {
+		log.Panic("This is PANIC.")
+		log.Fatal("This is FATAL.")
+	}
+
 	Library1Printer()
 }
