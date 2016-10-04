c75771595dd6497ad121e3efeb116e486baf3e99
Nate Tangsurat <e4r7hbug@gmail.com>
2016-10-04 01:10:48 -0500

    feat: External file with public function

---
+++ library1.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Library1Printer() {
+	fmt.Printf("Library 1 in service!\n")
+}
--- main.go
+++ main.go
@@ -6,4 +6,5 @@ import (
 
 func main() {
 	fmt.Printf("Hellow!\n")
+	Library1Printer()
 }
